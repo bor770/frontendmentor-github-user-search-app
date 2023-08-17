8ce542ee0b83ea68d7fbb9e7a1acb1baaacd14eb
Baruch Giverts <bor770@gmail.com>
2023-08-17 14:22:13 +0300

    base component

---
+++ src/app/shared/base/base.component.ts
@@ -0,0 +1,27 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+
+import { Theme } from '../theme/theme.model';
+import { Width } from '../layout/layout.model';
+import * as fromRoot from '../../store/app.reducer';
+
+@Component({
+  imports: [CommonModule],
+  selector: 'app-base',
+  standalone: true,
+  styleUrls: ['./base.component.css'],
+  templateUrl: './base.component.html',
+})
+export class BaseComponent implements OnInit {
+  theme: Observable<Theme>;
+  width: Observable<Width>;
+
+  constructor(public store: Store) {}
+
+  ngOnInit(): void {
+    this.theme = this.store.select(fromRoot.selectTheme);
+    this.width = this.store.select(fromRoot.selectLayoutWidth);
+  }
+}
